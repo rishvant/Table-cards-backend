069b98357ac619a75f31662b76642b763a14cc0c
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-03-13 16:05:04 +0530

    fix:model

--- models/user.js
+++ models/user.js
@@ -1,24 +1,9 @@
 import mongoose from "mongoose";
 
-function generateRandomNumber(length) {
-    let randomNumber = '';
-    for (let i = 0; i < length; i++) {
-        randomNumber += Math.floor(Math.random() * 10);
-    }
-    return parseInt(randomNumber);
-}
-
 const userSchema = mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.Mixed,
-        default: () => {
-            if (mongoose.Types.ObjectId) {
-                return new mongoose.Types.ObjectId();
-            } else {
-                return generateRandomNumber(8);
-            }
-        },
-        unique: true,
+        type: String,
+        unique: true
     },
     username: {
         type: String
